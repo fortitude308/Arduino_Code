11452345a47201cd61fbd46f102da3d79cd0b52a
HSJNYCU <hsj4nycu.ee10@nycu.edu.tw>
2023-04-03 14:08:57 +0800

    Update Madgwick_6Dof_HSJ.ino

--- Madgwick_6Dof_HSJ/Madgwick_6Dof_HSJ.ino
+++ Madgwick_6Dof_HSJ/Madgwick_6Dof_HSJ.ino
@@ -181,7 +181,14 @@ void loop() {
 }
 
 
+// Quaternion to Euler angle.
+//  roll = atan2f(q0*q1 + q2*q3, 0.5f - q1*q1 - q2*q2);
+//  pitch = asinf(-2.0f * (q1*q3 - q0*q2));
+//  yaw = atan2f(q1*q2 + q0*q3, 0.5f - q2*q2 - q3*q3);
 
+
+// Fast inverse square-root
+// http://en.wikipedia.org/wiki/Fast_inverse_square_root
 float invSqrt(float x) {
   float halfx = 0.5f * x;
   float y = x;
